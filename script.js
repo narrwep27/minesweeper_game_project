9867f4eb422d2e4a449e367af1658b3e62bb91ce
Narrwe <narrwep@gmail.com>
2021-11-18 16:38:35 -0500

    game can check number of mines on board and stops setting at 15% capacity

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 // Global constants
 const squareArray = document.querySelectorAll(`.square`);
-const mine = document.createElement(`img`);
-mine.setAttribute(`src`, `https://cdn.pixabay.com/photo/2017/01/31/16/59/bomb-2025548_1280.png`);
 
+// Functions for game logic
 // Function for random number generator from 0-15
 const RNG = () => {
     return Math.floor(Math.random() * squareArray.length)
 };
 // Function to place mine in random square
-const placeMine = () => {
+const createMine = () => {
     let randNum = RNG();
+    const mine = document.createElement(`img`);
+    mine.setAttribute(`src`, `https://cdn.pixabay.com/photo/2017/01/31/16/59/bomb-2025548_1280.png`);
+    mine.setAttribute(`class`, `mine`);
     squareArray[randNum].append(mine);
+};
+// Function to check number of mines in grid and set no more than 15% of board
+let minePercent = parseFloat(document.querySelectorAll(`.mine`).length / squareArray.length);
+const setMines = () => {
+    while (minePercent < .15) {
+        createMine();
+        minePercent = parseFloat(document.querySelectorAll(`.mine`).length / squareArray.length);
+    };
 };

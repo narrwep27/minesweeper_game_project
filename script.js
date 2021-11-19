28785910b72f1413fe107986d6e2615b1799e5f2
Narrwe <narrwep@gmail.com>
2021-11-19 13:31:38 -0500

    added functions to check number of adjacent bombs and show number in square

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 // Global constants
 const squareArray = document.querySelectorAll(`.square`);
+const mineNumArray = [];
+mineNumArray.length = squareArray.length;
+mineNumArray.fill(0);
+
+const row1Array = document.querySelectorAll(`.row1`);
+const row2Array = document.querySelectorAll(`.row2`);
+const row3Array = document.querySelectorAll(`.row3`);
+const row4Array = document.querySelectorAll(`.row4`);
+const row5Array = document.querySelectorAll(`.row5`);
+
 
 // Functions for game logic
 // Function for random number generator from 0-15
 const RNG = () => {
     return Math.floor(Math.random() * squareArray.length)
 };
-// Function to place mine in random square
+// Function to create and place mine in random square
 const createMine = () => {
     let randNum = RNG();
     const mine = document.createElement(`img`);
@@ -26,3 +36,68 @@ const setMines = () => {
     };
 };
 // Function for empty squares to check number of mines adjacent to it
+const row1CornerCount = () => {
+    for (i = 0; i < row1Array.length; i++) {
+        if (row1Array[i].classList[1] === `empty`) {
+            if (row1Array[i].classList[3] === `col1`) {
+                if (row1Array[i + 1].classList[1] === `mineHere`) {
+                    mineNumArray[0] += 1;
+                };
+                if (row2Array[i].classList[1] === `mineHere`) {
+                    mineNumArray[0] += 1;
+                };
+                if (row2Array[i + 1].classList[1] === `mineHere`) {
+                    mineNumArray[0] += 1;
+                };
+                const r1c1Text = document.createElement(`h3`);
+                r1c1Text.innerHTML = mineNumArray[0];
+                row1Array[i].append(r1c1Text);
+            };
+            if (row1Array[i].classList[3] === `col5`) {
+                if (row1Array[i - 1].classList[1] === `mineHere`) {
+                    mineNumArray[4] += 1;
+                };
+                if (row2Array[i - 1].classList[1] === `mineHere`) {
+                    mineNumArray[4] += 1;
+                };
+                if (row2Array[i].classList[1] === `mineHere`) {
+                    mineNumArray[4] += 1;
+                };
+                const r1c5Text = document.createElement(`h3`);
+                r1c5Text.innerHTML = mineNumArray[4];
+                row1Array[i].append(r1c5Text);
+            };
+        };
+    };
+};
+const row1CenterCountTest = (par1, par2) => {
+    for (i = 0; i < row1Array.length; i++) {
+        if (row1Array[i].classList[1] === `empty`) {
+            if (row1Array[i].classList[3] === par2) {
+                if (row1Array[i - 1].classList[1] === `mineHere`) {
+                    par1 += 1;
+                };
+                if (row1Array[i + 1].classList[1] === `mineHere`) {
+                    par1 += 1;
+                };
+                if (row2Array[i - 1].classList[1] === `mineHere`) {
+                    par1 += 1;
+                };
+                if (row2Array[i].classList[1] === `mineHere`) {
+                    par1 += 1;
+                };
+                if (row2Array[i + 1].classList[1] === `mineHere`) {
+                    par1 += 1;
+                };
+                let mineNum = document.createElement(`h3`);
+                mineNum.innerHTML = par1;
+                row1Array[i].append(mineNum);
+            };
+        };
+    };
+};
+row1CornerCount();
+row1CenterCountTest(mineNumArray[1], `col2`);
+row1CenterCountTest(mineNumArray[2], `col3`);
+row1CenterCountTest(mineNumArray[3], `col4`);
+console.log(mineNumArray);

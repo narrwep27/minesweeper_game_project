d3f679d3c1947f5b98b5fd1a90f9b2b612c71106
narrwep27 <narrwep@gmail.com>
2022-10-29 11:16:23 -0400

    renamed BeginnerPage.test.tsx

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import GameBoard from '../components/GameBoard';
 
-test('renders learn react link', () => {
+test('renders GameBoard component in App.tsx', () => {
   render(<App />);
 });
